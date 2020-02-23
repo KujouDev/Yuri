56ec6fca4fb5e3bc13512b431c892fd2c2a889d0
Rouven <revexderartist@gmail.com>
2020-02-23 21:25:59 +0100

    Last Update - Development has been stopped

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-// I know this script currently has some issues they need to get fixed
-// ~ Yuri Dev (Kujou)
-
 const	Discord	= require('discord.js'),
 		fs		= require('fs'),
 		config	= JSON.parse(fs.readFileSync('config.json', 'utf8')),
@@ -8,7 +5,7 @@ const	Discord	= require('discord.js'),
 
 client.on('ready', () => {
 	console.log(`Bot ${client.user.username} has logged in successfully...`)
-	client.user.setActivity(config.game, {url: "https://www.twitch.tv/kujxu", type: "LISTENING"}) // PLAYING LISTENING WATCHING
+	client.user.setActivity(config.game, {url: "https://www.twitch.tv/nxtrevex", type: "WATCHING"}) // PLAYING LISTENING WATCHING STREAMING
 	client.user.setStatus(config.status)
 });
 
@@ -19,7 +16,7 @@ client.on('message', (msg) => { // Message Event
 		guild	= msg.guild
 		trigger = cont.toLowerCase()
 
-	//if (msg.author.bot) return; // If the bot is the author, returns.
+	if (msg.author.bot) return; // Prevents Yuri from interacting with other bots
 	
 	if (author.id != client.user.id && cont.startsWith(config.prefix)) {
 		// Command wird gesplittet Ohne Split Bsp: y!say hello world!
@@ -41,7 +38,7 @@ client.on('message', (msg) => { // Message Event
 		// Load Commands from folder
 		fs.access(`./commands/${command}.js`, fs.constants.F_OK, (err) => {
 			if (err) {
-				console.log(`${msg.author.tag} tried to use ${command} command`);
+				// LOG GOT REMOVED BECAUSE IT'S SUPERFLUOUS
 			} else {
 				var commands = require(`./commands/${command}.js`); // Run command folder
 				commands.run(client, msg, args); // Runs [Client, Message, Args]
@@ -57,7 +54,8 @@ client.on('message', (msg) => { // Message Event
 		// Load Commands from folder
 		fs.access(`./triggers/${trigger}.js`, fs.constants.F_OK, (err) => {
 			if (err) {
-				console.log(`${msg.author.tag} tried to use ${trigger} trigger`);
+				// LOG GOT REMOVED DUE TO LACK OF PRIVACY AND FOR SAME REASON LIKE ERROR-LOG IN COMMAND-MODULE
+				// MORE ABOUT THE PRIVACY ISSUE: https://yuribot.xyz/twitter/status/1100429571416092672?s=20
 			} else {
 				var triggers = require(`./triggers/${trigger}.js`); // Run command folder
 				triggers.run(client, msg, args); // Runs [Client, Message, Args]
